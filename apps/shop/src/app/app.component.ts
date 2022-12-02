68ee1f246e1486ce822b8cfafd4b284402b973f6
Thuy Dinh <thuy.dinh@nng.bz>
2022-12-02 14:35:11 +0700

    init project

--- apps/shop/src/app/app.component.ts
+++ apps/shop/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'studying-nx-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
